76286f263340511016314bea55d4381e8e49cc2e
aarontimana04 <112721918+aarontimana04@users.noreply.github.com>
2024-07-15 15:15:08 -0500

    Update CrearTarea.py

--- CrearTarea.py
+++ CrearTarea.py
@@ -5,38 +5,39 @@ import boto3
 
 def lambda_handler(event, context):
     # Generar datos aleatorios para la tarea ficticia
-    task_id = str(random.randint(1000, 9999))  # Generar un ID de tarea aleatorio de 4 dígitos
-    task_name = f"Tarea {task_id}"  # Nombre de la tarea basado en el ID generado
-    task_description = f"Descripción de la Tarea {task_id}"  # Descripción de la tarea basada en el ID generado
-    task_status = random.choice(['Pendiente', 'En Progreso', 'Completada'])  # Estado aleatorio para la tarea
-    
-    # Generar una marca de tiempo para la creación de la tarea
+    task_id = str(random.randint(2505, 2510))  # Generar un ID de tarea aleatorio de 4 dígitos
+    tenant_id = "CS" + task_id
+    tarea_id = "T-"+str(random.randint(1, 10))
     now = datetime.now()
-    created_at = str(now.date()) + "." + str(now.time())
-    
-    # Crear el objeto de tarea
-    task_object = {
-        'task_id': task_id,
-        'task_name': task_name,
-        'task_description': task_description,
-        'task_status': task_status,
-        'created_at': created_at
+    descripcion = f"Tarea Final del curso {tenant_id}."
+    task_status = random.choice(['Pendiente', 'En Progreso', 'Completada'])  # Estado aleatorio para la tarea
+    fecha_hora = str(now.date()) + "." + str(now.time())
+
+    asignacion = {
+        'tenant_id': tenant_id,
+        'tarea_id': tarea_id,
+        'fecha_creacion': fecha_hora,
+        'descripcion': descripcion,
+        'status': task_status
     }
     
+    print(asignacion)  # Para logs en Cloud Watch
+
     # Publicar en SNS
     sns_client = boto3.client('sns')
     response_sns = sns_client.publish(
-        TopicArn='arn:aws:sns:us-east-1:864755325173:NuevaTarea',  # ARN del tema SNS para tareas
+        TopicArn='arn:aws:sns:us-east-1:864755325173:NuevaTarea',
         Subject='Nueva Tarea Creada',
-        Message=json.dumps(task_object),
+        Message=json.dumps(asignacion),
         MessageAttributes={
-            'task_id': {'DataType': 'String', 'StringValue': task_id},
-            'task_name': {'DataType': 'String', 'StringValue': task_name},
-            'task_status': {'DataType': 'String', 'StringValue': task_status}
+            'tenant_id': {'DataType': 'String', 'StringValue': tenant_id},
+            'tarea_id': {'DataType': 'String', 'StringValue': tarea_id},
+            'fecha_creacion': {'DataType': 'String', 'StringValue': fecha_hora},
+            'descripcion': {'DataType': 'String', 'StringValue': descripcion},
+            'status': {'DataType': 'String', 'StringValue': task_status}
         }
     )
     
-    # Retornar la respuesta
     return {
         'statusCode': 200,
         'body': response_sns
